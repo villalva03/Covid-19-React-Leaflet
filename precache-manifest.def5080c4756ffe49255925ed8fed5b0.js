64c8990aa57925fe4373851fa9987b5e4e2cdf1e
villalva03 <villalva03@gmail.com>
2021-01-20 14:34:02 -0300

    Updates

---
+++ precache-manifest.def5080c4756ffe49255925ed8fed5b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a68f678dcac1fa5c88f8457e2b2e6d",
+    "url": "/Covid-19-React-Leaflet/index.html"
+  },
+  {
+    "revision": "b98b4f8f11bc639b2f1a",
+    "url": "/Covid-19-React-Leaflet/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "80f1032c4ba138ce38f3",
+    "url": "/Covid-19-React-Leaflet/static/css/main.ebb2d263.chunk.css"
+  },
+  {
+    "revision": "b98b4f8f11bc639b2f1a",
+    "url": "/Covid-19-React-Leaflet/static/js/2.47f18041.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/Covid-19-React-Leaflet/static/js/2.47f18041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f1032c4ba138ce38f3",
+    "url": "/Covid-19-React-Leaflet/static/js/main.697d50f5.chunk.js"
+  },
+  {
+    "revision": "c1ab427ad03aa32a1cc8",
+    "url": "/Covid-19-React-Leaflet/static/js/runtime-main.539042b7.js"
+  }
+]);
